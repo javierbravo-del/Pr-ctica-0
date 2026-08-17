5b5ccf657337f6424315d07e9b40ab68af7d09c7
Javier Bravo <javier.bravo@utec.edu.pe>
2026-08-17 17:24:49 -0500

    Segundo commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+javie@Xavi MINGW64 ~/OneDrive/Escritorio/Nuevoproyecto (main)
+$ git push -u origin main
+Enumerating objects: 3, done.
+Counting objects: 100% (3/3), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 229 bytes | 229.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To github.com:javierbravo-del/Pr-ctica-0.git
+ * [new branch]      main -> main
+branch 'main' set up to track 'origin/main'.
+print("Hola mundo")
+
